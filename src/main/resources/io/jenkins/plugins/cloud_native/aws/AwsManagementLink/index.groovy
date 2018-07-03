205dea72193ad8c5c8045a4d9c6e83724a2979cc
Oleg Nenashev <o.v.nenashev@gmail.com>
2018-07-03 18:25:06 +0200

    Add AWS Management link to the /manage page

---
+++ src/main/resources/io/jenkins/plugins/cloud_native/aws/AwsManagementLink/index.groovy
@@ -0,0 +1,44 @@
+package hudson.security.GlobalSecurityConfiguration
+
+import hudson.security.SecurityRealm
+import hudson.markup.MarkupFormatterDescriptor
+import hudson.security.AuthorizationStrategy
+import jenkins.AgentProtocol
+import jenkins.model.GlobalConfiguration
+import hudson.Functions
+import hudson.model.Descriptor
+
+def f=namespace(lib.FormTagLib)
+def l=namespace(lib.LayoutTagLib)
+def st=namespace("jelly:stapler")
+
+l.layout(norefresh:true, permission:app.ADMINISTER, title:my.displayName, cssclass:request.getParameter('decorate')) {
+    l.main_panel {
+        h1 {
+            l.icon(class: 'icon-secure icon-xlg')
+            text(my.displayName)
+        }
+
+        p()
+        div(class:"behavior-loading", _("LOADING"))
+        f.form(method:"post",name:"config",action:"configure") {
+            set("instance",my);
+            set("descriptor", my.descriptor);
+
+            Functions.getSortedDescriptorsForGlobalConfig(my.FILTER).each { Descriptor descriptor ->
+                set("descriptor",descriptor)
+                set("instance",descriptor)
+                f.rowSet(name:descriptor.jsonSafeClassName) {
+                    st.include(from:descriptor, page:descriptor.globalConfigPage)
+                }
+            }
+
+            f.bottomButtonBar {
+                f.submit(value:_("Save"))
+                f.apply()
+            }
+        }
+
+        st.adjunct(includes: "lib.form.confirm")
+    }
+}
