88d8abef91b969bd190a6fa594e43020dabf21f1
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2026-01-28 15:02:48 +0000

    Update interface for experimental Manage Jenkins UI

--- src/main/resources/io/jenkins/plugins/aws/global_configuration/AwsManagementLink/index.groovy
+++ src/main/resources/io/jenkins/plugins/aws/global_configuration/AwsManagementLink/index.groovy
@@ -12,32 +12,21 @@ def f=namespace(lib.FormTagLib)
 def l=namespace(lib.LayoutTagLib)
 def st=namespace("jelly:stapler")
 
-l.layout(norefresh:true, permission:app.ADMINISTER, title:my.displayName, cssclass:request2.getParameter('decorate')) {
-    l.main_panel {
-        h1 {
-            text(my.displayName)
-        }
+l.'settings-subpage'(permission:app.ADMINISTER) {
+    f.form(method:"post",name:"config",action:"configure") {
+        set("instance",my);
+        set("descriptor", my.descriptor);
 
-        p()
-        div(class:"behavior-loading", _("LOADING"))
-        f.form(method:"post",name:"config",action:"configure") {
-            set("instance",my);
-            set("descriptor", my.descriptor);
-
-            Functions.getSortedDescriptorsForGlobalConfig(my.FILTER).each { Descriptor descriptor ->
-                set("descriptor",descriptor)
-                set("instance",descriptor)
-                f.rowSet(name:descriptor.jsonSafeClassName) {
-                    st.include(from:descriptor, page:descriptor.globalConfigPage)
-                }
-            }
-
-            f.bottomButtonBar {
-                f.submit(value:_("Save"))
-                f.apply()
+        Functions.getSortedDescriptorsForGlobalConfig(my.FILTER).each { Descriptor descriptor ->
+            set("descriptor",descriptor)
+            set("instance",descriptor)
+            f.rowSet(name:descriptor.jsonSafeClassName) {
+                st.include(from:descriptor, page:descriptor.globalConfigPage)
             }
         }
 
-        st.adjunct(includes: "lib.form.confirm")
+        f.saveApplyBar()
     }
+
+    st.adjunct(includes: "lib.form.confirm")
 }
